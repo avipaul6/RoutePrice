a7e2eb79ec732f364ad1bac067a77cc495d759fe
Avijit Paul <avijit.paul.1992@gmail.com>
2016-10-06 17:12:37 +1000

    Changed converter to print agent names with ID and price. also did More cleanup.

--- collector/list_all_routes.py
+++
@@ -1,5 +0,0 @@
-import csv
-with open('../input/route_list.csv', 'rt') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print (row)
--- converter/convert_json.py
+++ converter/convert_json.py
@@ -1,14 +1,30 @@
 import json
 from pprint import pprint
 
-with open('../output/SYD_LHR.json') as data_file:    
+with open('../output/MEL-HKT-2016-11-10-2016-11-24.json') as data_file:    
     data = json.load(data_file)
 
-for each_itineraries in data['Itineraries']:
-		# pprint (each_itineraries['PricingOptions'])
-	for each_pricing_options in each_itineraries['PricingOptions']:
-		print (each_pricing_options['Agents'], each_pricing_options['Price'])
+
+agent_dictionary= {}
+for each_agent in data['Agents']:
+	agent_dictionary.update({each_agent['Id'] : each_agent['Name']})
+
+print (agent_dictionary)
+
+def agent_finder(agent_id):
+	agent_name = None
+	if agent_id in agent_dictionary:
+		agent_name = agent_dictionary.get(agent_id)
+	return agent_name
 
 
-# for each_agent in data['Agents']:
-# 	print (each_agent['Id'], each_agent['Name'])
+
+def price_finder():
+	for each_itineraries in data['Itineraries']:
+			# pprint (each_itineraries['PricingOptions'])
+		for each_pricing_options in each_itineraries['PricingOptions']:
+			print (each_pricing_options['Agents'][0], agent_finder(each_pricing_options['Agents'][0]), each_pricing_options['Price'])
+
+
+price_finder()
+
